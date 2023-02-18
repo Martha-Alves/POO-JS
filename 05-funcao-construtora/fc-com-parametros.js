9501d821cd198e089a3b3c6f704ed968898d32fb
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-18 17:37:12 -0300

    adicionando teste com funcao construtora

---
+++ 05-funcao-construtora/fc-com-parametros.js
@@ -0,0 +1,9 @@
+function Animal(name, color, loves) {
+    this.nome = name;
+    this.cor= color;
+    this.ama = loves;
+}
+
+let data = new Animal("Pérola", "Preto, branco e marrom", "Comer");
+
+console.log(data)
