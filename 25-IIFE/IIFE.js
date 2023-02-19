d78b274079ae54bb9c1ec64f5f0e429e805bab6b
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-18 22:01:00 -0300

    adicionando vigesimo quinto exercicio

---
+++ 25-IIFE/IIFE.js
@@ -0,0 +1,5 @@
+//Este padrão é conhecido como immediately invoked function expression (expressão de função invocada imediatamente) ou IIFE.
+
+(function () {
+    console.log("A cozy nest is ready");
+  })();
