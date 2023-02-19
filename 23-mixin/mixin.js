6ecb71a0d8eec1be33f0a861fc85967ae6637751
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-02-18 21:53:49 -0300

    adicionando vigesimo terceiro exercicio

---
+++ 23-mixin/mixin.js
@@ -0,0 +1,21 @@
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Altere apenas o código abaixo desta linha
+  
+  let glideMixin = function(obj) {
+    obj.glide = function() {
+      console.log("deslizando")
+    };
+  }
+  
+  glideMixin(bird);
+  glideMixin(boat);
+  
